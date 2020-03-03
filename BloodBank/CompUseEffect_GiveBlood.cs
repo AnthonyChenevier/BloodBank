d17f51442f3323b1e46e0f2fa283d6fe89b35c22
Anthony Chenevier <anthony.chenevier@gmail.com>
2020-03-03 22:55:44 +1100

    Readd solution

---
+++ BloodBank/CompUseEffect_GiveBlood.cs
@@ -0,0 +1,38 @@
+﻿using RimWorld;
+using Verse;
+
+namespace BloodBank
+{
+    public class CompProperties_Blood : CompProperties_UseEffect
+    {
+        public float bloodAmount;
+        public float minSeverityForGive;
+        public float harvestEfficiencyFactor;
+        public float minSeverityForBadThought;
+
+        public CompProperties_Blood() { compClass = typeof(CompUseEffect_GiveBlood); }
+    }
+
+    public class CompUseEffect_GiveBlood : CompUseEffect
+    {
+        private CompProperties_Blood Props => (CompProperties_Blood)props;
+
+        public override bool CanBeUsedBy(Pawn p, out string failReason)
+        {
+            if (!p.health.hediffSet.HasHediff(HediffDefOf.BloodLoss) ||
+                p.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.BloodLoss).Severity <= Props.minSeverityForGive)
+            {
+                failReason = p.Name.ToStringShort + " does not need blood";
+                return false;
+            }
+
+            return base.CanBeUsedBy(p, out failReason);
+        }
+
+        public override void DoEffect(Pawn pawn)
+        {
+            BloodBankUtilities.ApplyBloodPack(pawn, parent.def);
+            base.DoEffect(pawn);
+        }
+    }
+}
---
+++ BloodBank/ThingDefGenerator_Blood.cs
@@ -0,0 +1,184 @@
+﻿// ThingDefGenerator_Blood.cs
+// 
+// Part of BloodBank - BloodBank
+// 
+// Created by: Anthony Chenevier on // 
+// Last edited by: Anthony Chenevier on 2020/02/06 3:34 PM
+
+
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace BloodBank {
+    public static class ThingDefGenerator_Blood {
+        public static IEnumerable<ThingDef> ImpliedBloodDefs()
+        {
+            foreach (ThingDef sourceDef in DefDatabase<ThingDef>.AllDefs.ToList())
+            {
+                if (sourceDef.category != ThingCategory.Pawn || sourceDef.race.useMeatFrom != null || !sourceDef.race.IsFlesh)
+                    continue;
+
+
+                ThingDef bloodDef = new ThingDef();
+                bloodDef.resourceReadoutPriority = ResourceCountPriority.Middle;
+                bloodDef.category = ThingCategory.Item;
+                bloodDef.thingClass = typeof(ThingWithComps);
+                bloodDef.useHitPoints = true;
+                bloodDef.selectable = true;
+                bloodDef.altitudeLayer = AltitudeLayer.Item;
+                bloodDef.stackLimit = 25;
+
+                bloodDef.SetStatBaseValue(StatDefOf.Beauty, -4f);
+                bloodDef.SetStatBaseValue(StatDefOf.MaxHitPoints, 60f);
+                bloodDef.SetStatBaseValue(StatDefOf.DeteriorationRate, 10f);
+                bloodDef.SetStatBaseValue(StatDefOf.Mass, 0.5f);
+                bloodDef.SetStatBaseValue(StatDefOf.Flammability, 2f);
+                bloodDef.SetStatBaseValue(StatDefOf.Nutrition, 0.01f);
+                bloodDef.SetStatBaseValue(StatDefOf.FoodPoisonChanceFixedHuman, 0.02f);
+
+                bloodDef.tickerType = TickerType.Rare;
+
+                bloodDef.comps.Add(new CompProperties_Forbiddable());
+                bloodDef.comps.Add(new CompProperties_Rottable
+                {
+                    daysToRotStart = 0.5f, rotDestroys = true
+                });
+                bloodDef.comps.Add(new CompProperties_Blood
+                {
+                    bloodAmount = 0.1f,
+                    minSeverityForGive = 0.1f,
+                    minSeverityForBadThought = 0.4f,
+                    harvestEfficiencyFactor = 2f
+                });
+
+                //humanlike blood can be self-administered (if same race)
+                if (sourceDef.race.Humanlike)
+                {
+                    bloodDef.comps.Add(new CompProperties_Usable
+                    {
+                        useJob = DefDatabase<JobDef>.GetNamed("UseBloodProduct"),
+                        useLabel = "GiveSelfTransfusion".Translate()
+                    });
+                    bloodDef.comps.Add(new CompProperties_UsableWithSkill
+                    {
+                        skillRequirements = new List<SkillRequirement>
+                        {
+                            new SkillRequirement { skill = SkillDefOf.Medicine, minLevel = 4 }
+                        }
+                    });
+                    bloodDef.comps.Add(new CompProperties_UsableByRace { allowedRaces = new List<ThingDef>{ sourceDef } });
+                    bloodDef.comps.Add(new CompProperties_UseEffect { compClass = typeof(CompUseEffect_GiveBlood) });
+                    bloodDef.comps.Add(new CompProperties_UseEffect { compClass = typeof(CompUseEffect_DestroySelf) });
+                }
+
+                bloodDef.alwaysHaulable = true;
+                bloodDef.drawGUIOverlay = true;
+                bloodDef.rotatable = false;
+                bloodDef.pathCost = 15;
+
+                if (bloodDef.thingCategories == null)
+                {
+                    bloodDef.thingCategories = new List<ThingCategoryDef>();
+                }
+
+                DirectXmlCrossRefLoader.RegisterListWantsCrossRef(bloodDef.thingCategories, "RawBlood", bloodDef);
+
+                bloodDef.uiIconForStackCount = 1;
+                bloodDef.soundInteract = SoundDefOf.Standard_Drop;
+                bloodDef.soundDrop = SoundDefOf.Standard_Drop;
+
+                bloodDef.techLevel = TechLevel.Industrial;
+
+                bloodDef.socialPropernessMatters = true;
+                bloodDef.modContentPack = sourceDef.modContentPack; //does this matter?
+
+                //worth 4x as much as the meat of the same animal if humanlike, half as much for critters
+                bloodDef.BaseMarketValue = sourceDef.race.Humanlike
+                                                   ? sourceDef.race.meatMarketValue * 4f
+                                                   : sourceDef.race.meatMarketValue / 2f;
+
+                bloodDef.graphicData = new GraphicData
+                {
+                    graphicClass = typeof(Graphic_StackCount),
+                    texPath = sourceDef.race.FleshType == FleshTypeDefOf.Insectoid
+                                      ? "Things/Item/Resource/InsectBloodPack"
+                                      : "Things/Item/Resource/BloodPack",
+                    drawSize = Vector2.one * 0.85f
+                };
+
+                if (sourceDef.race.Humanlike)
+                    bloodDef.description = "BloodHumanlikeDesc".Translate(sourceDef.label);
+                else if (sourceDef.race.FleshType == FleshTypeDefOf.Insectoid)
+                    bloodDef.description = "BloodInsectDesc".Translate(sourceDef.label);
+                else
+                    bloodDef.description = "BloodDesc".Translate(sourceDef.label);
+
+
+                bloodDef.ingestible = new IngestibleProperties
+                {
+                    parent = bloodDef,
+                    foodType = FoodTypeFlags.Fluid | FoodTypeFlags.AnimalProduct,
+                    preferability = FoodPreferability.DesperateOnlyForHumanlikes,
+                    ingestCommandString = "Drink {0}",
+                    ingestReportString = "Drinking {0}.",
+                    ingestEffect = EffecterDefOf.EatMeat,
+                    ingestSound = SoundDef.Named("Ingest_Drink"),
+                    sourceDef = sourceDef,
+                    specialThoughtDirect = sourceDef.race.Humanlike
+                                                ? ThoughtDef.Named("DrankHumanlikeBlood")
+                                                : sourceDef.race.FleshType == FleshTypeDefOf.Insectoid 
+                                                        ? ThoughtDef.Named("ConsumedInsectHemolymph")
+                                                        : null,
+                    specialThoughtAsIngredient = sourceDef.race.Humanlike
+                                                ? ThoughtDef.Named("ConsumedHumanlikeBloodAsIngredient")
+                                                : sourceDef.race.FleshType == FleshTypeDefOf.Insectoid
+                                                        ? ThoughtDef.Named("ConsumedInsectHemolymphAsIngredient")
+                                                        : null,
+                };
+
+                DirectXmlCrossRefLoader.RegisterObjectWantsCrossRef(bloodDef.ingestible, "tasteThought", "DrankRawBlood");
+
+                bloodDef.defName = "Blood_" + sourceDef.defName;
+
+
+                if (sourceDef.race.FleshType == FleshTypeDefOf.Insectoid)
+                {
+                    bloodDef.label = "InsectBloodLabel".Translate();
+                }
+                else
+                {
+                    //HACKY STUFF INCOMING
+                    //meat defs are created based on 3 exclusive rules. 
+                    //1. def.race.useMeatFrom -> just take the meat def from this source
+                    //2. def.race.meatLabel -> use this label instead of generating one. Used for
+                    //   two reasons: a. overriding names of common meat types (beef, venison, pork)
+                    //   and b. generating a collective meat name for base types used in rule 1.
+                    //3. Generate the name of the meat as 'def.label + " meat"'
+                    //blood defs need to follow a similar principle, but without using the common meat name overrides.
+                    //also needs to be translation compatible.
+
+                    string meatLabel = sourceDef.race.meatLabel;
+                    //check for collective meat type (2b). These should be of the form '<race> meat'
+                    //(contains a space) - Yaks are a known exception with 'yak beef', but this should
+                    //handle that and similar situations as long as the first part is still the race name.
+                    if (!meatLabel.NullOrEmpty() && meatLabel.Contains(' '))
+                    {
+                        //cut out the part before the space, hopefully that's the <race> part. 
+                        bloodDef.label = "BloodLabel".Translate(meatLabel.Substring(0, meatLabel.IndexOf(' ')));
+                    }
+                    else
+                    {
+                        //no meat label, or meat label is for common meat name. just use the source def label
+                        bloodDef.label = "BloodLabel".Translate(sourceDef.label);
+                    }
+                }
+
+
+                yield return bloodDef;
+            }
+        }
+    }
+}
